a7bf4ed4fb799e2efce4c7ed4e0cb9b494a46773
Anton Vasiliev <aionbase@gmail.com>
2022-12-03 15:44:18 +0000

    Further implementation of `Request` class

--- src/Request.php
+++ src/Request.php
@@ -15,6 +15,7 @@ namespace Phalcon\Bridge\Swoole;
 
 use Phalcon\Di\AbstractInjectionAware;
 use Phalcon\Di\InjectionAwareInterface;
+use Phalcon\Filter\FilterInterface;
 use Phalcon\Http\Message\RequestMethodInterface;
 use Phalcon\Http\RequestInterface;
 use Phalcon\Http\Request\Exception;
@@ -24,14 +25,63 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
 {
     private SwooleRequest $swooleRequest;
 
+    /**
+     * Emulation of `$_REQUEST` variable.
+     *
+     * @var array
+     */
+    private array $combinedRequest;
+
+    /**
+     * @var FilterInterface|null
+     */
+    private ?FilterInterface $filterService = null;
+
     public function __construct(SwooleRequest $swooleRequest)
     {
         $this->swooleRequest = $swooleRequest;
+
+        /**
+         * Adjust default `null` value to empty array.
+         */
+        $this->swooleRequest->get = $this->swooleRequest->get ?: [];
+        $this->swooleRequest->post = $this->swooleRequest->post ?: [];
+        $this->swooleRequest->cookie = $this->swooleRequest->cookie ?: [];
+
+        $this->combinedRequest = array_merge(
+            $this->swooleRequest->get,
+            $this->swooleRequest->post,
+            $this->swooleRequest->cookie,
+        );
     }
 
-    public function get(string $name = null, $filters = null, $defaultValue = null, bool $notAllowEmpty = false, bool $noRecursive = false)
-    {
-        // TODO: Implement get() method.
+    /**
+     * Gets a variable from the $_REQUEST superglobal applying filters if
+     * needed. If no parameters are given the $_REQUEST superglobal is returned
+     *
+     *```php
+     * // Returns value from $_REQUEST["user_email"] without sanitizing
+     * $userEmail = $request->get("user_email");
+     *
+     * // Returns value from $_REQUEST["user_email"] with sanitizing
+     * $userEmail = $request->get("user_email", "email");
+     *
+     * @param string|null $name
+     * @param null $filters
+     * @param null $defaultValue
+     * @param bool $notAllowEmpty
+     * @param bool $noRecursive
+     * @return mixed
+     * @throws Exception
+     */
+    public function get(
+        string $name = null,
+        $filters = null,
+        $defaultValue = null,
+        bool $notAllowEmpty = false,
+        bool $noRecursive = false
+    ): mixed {
+        return $this->getHelper($this->combinedRequest, $name, $filters, $defaultValue, $notAllowEmpty, $noRecursive);
     }
 
     public function getAcceptableContent(): array
@@ -41,7 +91,14 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
 
     public function getBasicAuth(): ?array
     {
-        // TODO: Implement getBasicAuth() method.
+        if (!$this->hasServer('php_auth_user') || !$this->hasServer('php_auth_pw')) {
+            return null;
+        }
+
+        return [
+            'username' => $this->getServer('php_auth_user'),
+            'password' => $this->getServer('php_auth_pw'),
+        ];
     }
 
     public function getBestAccept(): string
@@ -69,9 +126,12 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
         // TODO: Implement getClientCharsets() method.
     }
 
+    /**
+     * Gets content type which request has been made
+     */
     public function getContentType(): ?string
     {
-        // TODO: Implement getContentType() method.
+        return $this->getServerArray()['content_type'] ?? null;
     }
 
     public function getDigestAuth(): array
@@ -79,9 +139,15 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
         // TODO: Implement getDigestAuth() method.
     }
 
+    /**
+     * Gets HTTP header from request data
+     */
     public function getHeader(string $header): string
     {
-        // TODO: Implement getHeader() method.
+        $name = strtolower(strtr($header, '_', '-'));
+        $server = $this->getServerArray();
+
+        return $server[$name] ?? '';
     }
 
     public function getHeaders(): array
@@ -148,24 +214,38 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
 
     public function getScheme(): string
     {
-        $https = $this->swooleRequest->server['HTTPS'] ?? '';
+        $https = $this->swooleRequest->server['https'] ?? '';
 
         return $https && $https !== "off" ? 'https' : 'http';
     }
 
+    /**
+     * Gets variable from $_SERVER superglobal
+     */
     public function getServer(string $name): ?string
     {
-        return $this->swooleRequest->get();
+        return $this->swooleRequest->server[$name] ?? null;
     }
 
+    /**
+     * Gets active server address IP
+     */
     public function getServerAddress(): string
     {
-        // TODO: Implement getServerAddress() method.
+        $serverAddress = $this->getServer('server_addr');
+        if (null === $serverAddress) {
+            return gethostbyname('localhost');
+        }
+
+        return $serverAddress;
     }
 
+    /**
+     * Gets active server name
+     */
     public function getServerName(): string
     {
-        // TODO: Implement getServerName() method.
+        return $this->getServer('server_name') ?: 'localhost';
     }
 
     public function getUploadedFiles(bool $onlySuccessful = false, bool $namedKeys = false): array
@@ -175,27 +255,27 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
 
     public function getUserAgent(): string
     {
-        // TODO: Implement getUserAgent() method.
+        return $this->getHeader('user-agent');
     }
 
     public function has(string $name): bool
     {
-        // TODO: Implement has() method.
+        return isset($this->combinedRequest[$name]);
     }
 
     public function hasFiles(): bool
     {
-        // TODO: Implement hasFiles() method.
+        return $this->numFiles(true) > 0;
     }
 
     public function hasHeader(string $header): bool
     {
-        // TODO: Implement hasHeader() method.
+        return isset($this->swooleRequest->header[$header]);
     }
 
     public function hasQuery(string $name): bool
     {
-        // TODO: Implement hasQuery() method.
+        return isset($this->swooleRequest->get[$name]);
     }
 
     public function hasPost(string $name): bool
@@ -213,9 +293,13 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
         return isset($this->swooleRequest->server[$name]);
     }
 
+    /**
+     * Checks whether request has been made using ajax
+     */
     public function isAjax(): bool
     {
-        // TODO: Implement isAjax() method.
+        return $this->hasServer('http_x_requested_with') &&
+            $this->getServer('http_x_requested_with') === 'XMLHttpRequest';
     }
 
     public function isConnect(): bool
@@ -238,14 +322,23 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
         return $this->getMethod() === self::METHOD_HEAD;
     }
 
+    /**
+     * Check if HTTP method match any of the passed methods
+     * When strict is true it checks if validated methods are real HTTP methods
+     *
+     * @param array|string $methods
+     * @param bool $strict
+     * @return bool
+     * @throws Exception
+     */
     public function isMethod($methods, bool $strict = false): bool
     {
         $httpMethod = $this->getMethod();
 
         if (is_string($methods)) {
-        if ($strict && !$this->isValidHttpMethod($methods)) {
-            throw new Exception("Invalid HTTP method: " . $methods);
-        }
+            if ($strict && !$this->isValidHttpMethod($methods)) {
+                throw new Exception("Invalid HTTP method: " . $methods);
+            }
 
             return $methods == $httpMethod;
         }
@@ -287,14 +380,24 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
         return $this->getMethod() === self::METHOD_PUT;
     }
 
+    /**
+     * Checks whether request has been made using any secure layer
+     */
     public function isSecure(): bool
     {
-        //$this->swooleRequest->
+        return $this->getScheme() === 'https';
     }
 
+    /**
+     * Checks whether request has been made using SOAP
+     */
     public function isSoap(): bool
     {
-        // TODO: Implement isSoap() method.
+        if ($this->hasServer('http_soapaction')) {
+            return true;
+        }
+
+        return strstr($this->getContentType(), 'application/soap+xml') !== false;
     }
 
     public function isTrace(): bool
@@ -303,14 +406,31 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
     }
 
     /**
-     * TODO: Implement
+     * Returns the number of files available
      *
      * @param bool $onlySuccessful
      * @return int
      */
     public function numFiles(bool $onlySuccessful = false): int
     {
-        return 0;
+        $numFiles = 0;
+
+        foreach ($this->swooleRequest->files as $file) {
+            if (!isset($file['error'])) {
+                continue;
+            }
+
+            $error = $file['error'];
+            if (!is_array($error)) {
+                if (!$error || !$onlySuccessful) {
+                    $numFiles++;
+                }
+            } else {
+                $numFiles += $this->hasFileHelper($error, $onlySuccessful);
+            }
+        }
+
+        return $numFiles;
     }
 
     /**
@@ -333,5 +453,108 @@ class Request extends AbstractInjectionAware implements RequestInterface, Reques
         }
 
         return false;
-}
+    }
+
+    /**
+     * Recursively counts file in an array of files
+     *
+     * @param mixed $data
+     * @param bool $onlySuccessful
+     * @return int
+     */
+    final protected function hasFileHelper(mixed $data, bool $onlySuccessful): int
+    {
+        if (!is_array($data)) {
+            return 1;
+        }
+
+        $numberFiles = 0;
+
+        foreach ($data as $value) {
+            if (!is_array($value)) {
+                if (!$value || !$onlySuccessful) {
+                    $numberFiles++;
+                }
+            } else {
+                $numberFiles += $this->hasFileHelper($value, $onlySuccessful);
+            }
+        }
+
+        return $numberFiles;
+    }
+
+    /**
+     * Helper to get data from superglobals, applying filters if needed.
+     * If no parameters are given the superglobal is returned.
+     *
+     * @param array $source
+     * @param string|null $name
+     * @param mixed|null $filters
+     * @param mixed|null $defaultValue
+     * @param bool $notAllowEmpty
+     * @param bool $noRecursive
+     * @return mixed
+     * @throws Exception
+     */
+    final protected function getHelper(
+        array   $source,
+        ?string $name = null,
+        mixed   $filters = null,
+        mixed   $defaultValue = null,
+        bool    $notAllowEmpty = false,
+        bool    $noRecursive = false
+    ): mixed
+    {
+        if ($name === null) {
+            return $source;
+        }
+
+        if (!isset($source[$name])) {
+            return $defaultValue;
+        }
+
+        $value = $source[$name];
+        if (!is_numeric($value) && empty($value) && $notAllowEmpty) {
+            return $defaultValue;
+        }
+
+        if ($filters !== null) {
+            $filterService = $this->getFilterService();
+            $value = $filterService->sanitize($value, $filters, $noRecursive);
+
+            /**
+             * @todo Leave this here for PHP 7.4/8.0. Remove when appropriate.
+             * Some filters use filter_var which can return `false`
+             */
+            if ($value === false) {
+                return $defaultValue;
+            }
+        }
+
+        return $value;
+    }
+
+    /**
+     * Checks the filter service and assigns it to the class parameter
+     * @throws Exception
+     */
+    private function getFilterService(): FilterInterface
+    {
+        if ($this->filterService !== null) {
+            return $this->filterService;
+        }
+
+        if ($this->container === null) {
+            throw new Exception(
+                "A dependency injection container is required to access the 'filter' service"
+            );
+        }
+
+        return $this->filterService = $this->container->getShared('filter');
+    }
+
+    private function getServerArray(): array
+    {
+        return $this->swooleRequest->header ?: [];
+    }
 }
